9e79b1020684c9c7ae45fd18eb2c5b85d0d0ce12
Cho Hyukeun <chohyukeun05@gmail.com>
2026-04-07 04:44:08 +0000

    new version

--- mydlist.hpp
+++ mydlist.hpp
@@ -9,6 +9,14 @@ private:
 	MyNode<T>* p;
 
 public:
+	MyNode()
+    	{
+        	e = nullptr;
+        	n = nullptr;
+        	p = nullptr;
+    	}
+
+
 	void set(T* in) { this->e = in; }
 	T* get() { return this->e; }
 	void setNext(MyNode<T>* newnext) { n = newnext; }
@@ -28,43 +36,106 @@ private:
 public:
 	MyDoublyLinkedList()
 	{
+		head = new MyNode<T>();
+        	tail = new MyNode<T>();
+        	head->setNext(tail);
+        	tail->setPrev(head);
+        	isize = 0;
 	}
 
 	~MyDoublyLinkedList()
 	{
+		while (!isEmpty())
+        	{
+            		removeFromHead();
+        	}
+        	delete head;
+        	delete tail;
 	}
 
 	bool isEmpty()
 	{
+		return isize == 0;
 	}
 
 	int getSize()
 	{
+		return isize;
 	}
 
 
 	MyNode<T>* getHeadNode()
 	{
+		return head;
 	}
 
 	MyNode<T>* getTailNode()
 	{
+		return tail;
 	}
 
 	void addToHead(T* in)
 	{
+		MyNode<T>* newNode = new MyNode<T>();
+        	MyNode<T>* first = head->getNext();
+
+        	newNode->set(in);
+        	newNode->setPrev(head);
+        	newNode->setNext(first);
+
+        	head->setNext(newNode);
+        	first->setPrev(newNode);
+
+        	isize++;
 	}
 
 	void removeFromHead()
 	{
+        	if (isEmpty())
+        	{
+            		return;
+        	}
+
+        	MyNode<T>* first = head->getNext();
+        	MyNode<T>* second = first->getNext();
+
+        	head->setNext(second);
+        	second->setPrev(head);
+
+        	delete first;
+        	isize--;
 	}
 
 
 	void addToTail(T* in)
 	{
+		MyNode<T>* newNode = new MyNode<T>();
+        	MyNode<T>* last = tail->getPrev();
+
+        	newNode->set(in);
+        	newNode->setNext(tail);
+        	newNode->setPrev(last);
+
+        	last->setNext(newNode);
+        	tail->setPrev(newNode);
+
+        	isize++;
 	}
 
 	void removeFromTail()
 	{
+		if (isEmpty())
+        	{
+           		 return;
+        	}
+
+        	MyNode<T>* last = tail->getPrev();
+        	MyNode<T>* beforeLast = last->getPrev();
+
+        	beforeLast->setNext(tail);
+        	tail->setPrev(beforeLast);
+
+        	delete last;
+        	isize--;
 	}
 };
